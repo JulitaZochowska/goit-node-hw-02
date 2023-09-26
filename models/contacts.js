7e6bcd0de6f780a7b0bb26e8898842ed773f73af
JulitaZochowska <julita.zochowska@outlook.com>
2023-09-26 20:57:08 +0200

    Fix hw-02 pr

--- models/contacts.js
+++ models/contacts.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const { nanoid } = require("nanoid");
 const path = require("path");
 
@@ -6,70 +6,97 @@ const path = require("path");
 const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  const data = fs.readFileSync(contactsPath);
-  return JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    return JSON.parse(data);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 const getContactById = async (contactId) => {
-  const data = fs.readFileSync(contactsPath);
-  const allContacts = JSON.parse(data);
-  const selectedContact = allContacts.find(
-    (contact) => contact.id === contactId
-  );
-  return selectedContact;
+  try {
+    const data = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(data);
+    const selectedContact = allContacts.find(
+      (contact) => contact.id === contactId
+    );
+    return selectedContact;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 const removeContact = async (contactId) => {
-  const data = fs.readFileSync(contactsPath);
-  const allContacts = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(data);
 
-  const foundContact = allContacts.find((contact) => contact.id === contactId); // Znaleziony kontakt do usunięcia
-  if (!foundContact) {
-    // Jeżeli nie ma kontaktu, zwracam false i oczywiście przerywamy działanie funkcji
-    return false;
+    const foundContact = allContacts.find(
+      (contact) => contact.id === contactId
+    ); // Znaleziony kontakt do usunięcia
+    if (!foundContact) {
+      // Jeżeli nie ma kontaktu, zwracam false i oczywiście przerywamy działanie funkcji
+      return false;
+    }
+
+    // Filtrujemy obecną listę kontaktów BEZ znalezionego kontaktu - otrzymujemy listę tak jakby z usuniętym kontaktem
+    const newContactsList = allContacts.filter(
+      (contact) => contact.id !== foundContact.id
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(newContactsList));
+    return true;
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
-
-  // Filtrujemy obecną listę kontaktów BEZ znalezionego kontaktu - otrzymujemy listę tak jakby z usuniętym kontaktem
-  const newContactsList = allContacts.filter(
-    (contact) => contact.id !== foundContact.id
-  );
-  fs.writeFileSync(contactsPath, JSON.stringify(newContactsList));
-  return true;
 };
 
 const addContact = async (body) => {
-  const data = fs.readFileSync(contactsPath);
-  const allContacts = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(data);
 
-  // body zawiera name, email i phone, więc wystarczy dodać do obiektu id aby utworzyć nowy kontakt
-  const newContact = { id: nanoid(), ...body };
-  allContacts.push(newContact);
+    // body zawiera name, email i phone, więc wystarczy dodać do obiektu id aby utworzyć nowy kontakt
+    const newContact = { id: nanoid(), ...body };
+    allContacts.push(newContact);
 
-  fs.writeFileSync(contactsPath, JSON.stringify(allContacts));
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
 
-  return newContact;
+    return newContact;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
 
 const updateContact = async (contactId, body) => {
-  const data = fs.readFileSync(contactsPath);
-  const allContacts = JSON.parse(data);
+  try {
+    const data = await fs.readFile(contactsPath);
+    const allContacts = JSON.parse(data);
 
-  const foundContactIndex = allContacts.findIndex(
-    (contact) => contact.id === contactId
-  ); // Znaleziony kontakt do usunięcia
+    const foundContactIndex = allContacts.findIndex(
+      (contact) => contact.id === contactId
+    ); // Znaleziony kontakt do usunięcia
 
-  if (foundContactIndex === -1) {
-    // Jeżeli nie ma kontaktu (funkcja findIndex zwraca -1), zwracam false i oczywiście przerywamy działanie funkcji
-    return false;
+    if (foundContactIndex === -1) {
+      // Jeżeli nie ma kontaktu (funkcja findIndex zwraca -1), zwracam false i oczywiście przerywamy działanie funkcji
+      return false;
+    }
+
+    // Na podstawie pozyskanego wcześniej indeksu w tablicy, modyfikujemy kontakt
+    allContacts[foundContactIndex].name = body.name;
+    allContacts[foundContactIndex].email = body.email;
+    allContacts[foundContactIndex].phone = body.phone;
+
+    await fs.writeFile(contactsPath, JSON.stringify(allContacts));
+    return allContacts[foundContactIndex];
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
-
-  // Na podstawie pozyskanego wcześniej indeksu w tablicy, modyfikujemy kontakt
-  allContacts[foundContactIndex].name = body.name;
-  allContacts[foundContactIndex].email = body.email;
-  allContacts[foundContactIndex].phone = body.phone;
-
-  fs.writeFileSync(contactsPath, JSON.stringify(allContacts));
-  return allContacts[foundContactIndex];
 };
 
 module.exports = {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -18,18 +18,26 @@ const {
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const contacts = [await listContacts()];
-  res.status(200).send({ contacts });
+  try {
+    const contacts = await listContacts();
+    res.status(200).send({ contacts });
+  } catch (e) {
+    res.status(500).send({ error: e });
+  }
 });
 
 router.get("/:contactId", async (req, res, next) => {
   // Params to są te po ukośniku w URI, np. http://adres:port/nazwa/funkcji/12345 - gdzie 12345 to contactId
   const { contactId } = req.params;
-  const contact = await getContactById(contactId);
-  if (contact) {
-    res.status(200).send({ contact });
-  } else {
-    res.status(404).send({ message: "Not found" });
+  try {
+    const contact = await getContactById(contactId);
+    if (contact) {
+      res.status(200).send({ contact });
+    } else {
+      res.status(404).send({ message: "Not found" });
+    }
+  } catch (e) {
+    res.status(500).send({ error: e });
   }
 });
 
@@ -44,19 +52,27 @@ router.post("/", async (req, res, next) => {
   if (validationResult.error) {
     res.status(400).json({ message: "missing required name - field" });
   } else {
-    // req.body zawiera dane, które są przekazywane jako body requestu typu POST lub innych typów - odpowiednik parametrów po znaku zapytania (query) albo po ukośniku (params)
-    // tylko pozwalają wprowadzić dużo więcej danych w różnym formacie
-    const newContact = await addContact(req.body);
-    res.status(201).json(newContact); // TODO: dodac wygenerowany obiekt kontaktu
+    try {
+      // req.body zawiera dane, które są przekazywane jako body requestu typu POST lub innych typów - odpowiednik parametrów po znaku zapytania (query) albo po ukośniku (params)
+      // tylko pozwalają wprowadzić dużo więcej danych w różnym formacie
+      const newContact = await addContact(req.body);
+      res.status(201).json(newContact); // TODO: dodac wygenerowany obiekt kontaktu
+    } catch (e) {
+      res.status(500).send({ error: e });
+    }
   }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  const isContactRemoved = await removeContact(req.params.contactId);
-  if (isContactRemoved) {
-    res.status(200).json({ message: "contact deleted" });
-  } else {
-    res.status(404).json({ message: "Not found" });
+  try {
+    const isContactRemoved = await removeContact(req.params.contactId);
+    if (isContactRemoved) {
+      res.status(200).json({ message: "contact deleted" });
+    } else {
+      res.status(404).json({ message: "Not found" });
+    }
+  } catch (e) {
+    res.status(500).send({ error: e });
   }
 });
 
@@ -65,11 +81,15 @@ router.put("/:contactId", async (req, res, next) => {
   if (validationResult.error) {
     res.status(400).json({ message: "missing fields" });
   } else {
-    const contact = await updateContact(req.params.contactId, req.body);
-    if (contact) {
-      res.status(200).send({ contact });
-    } else {
-      res.status(404).send({ message: "Not found" });
+    try {
+      const contact = await updateContact(req.params.contactId, req.body);
+      if (contact) {
+        res.status(200).send({ contact });
+      } else {
+        res.status(404).send({ message: "Not found" });
+      }
+    } catch (e) {
+      res.status(500).send({ error: e });
     }
   }
 });
