f6d82f1a8da590e0ba51d7894eb10bae5c87465a
JulitaZochowska <julita.zochowska@outlook.com>
2023-09-26 14:07:21 +0200

    Homework 2 done

--- models/contacts.js
+++ models/contacts.js
@@ -1,21 +1,76 @@
-// const fs = require('fs/promises')
-let contacts = [];
+const fs = require("fs");
+const { nanoid } = require("nanoid");
+const path = require("path");
+
+// tworzymy ścieżkę do pliku z danymi
+const contactsPath = path.join(__dirname, "contacts.json");
 
 const listContacts = async () => {
-  return contacts;
+  const data = fs.readFileSync(contactsPath);
+  return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
-  return contacts.find((contact) => contact.id === id);
+  const data = fs.readFileSync(contactsPath);
+  const allContacts = JSON.parse(data);
+  const selectedContact = allContacts.find(
+    (contact) => contact.id === contactId
+  );
+  return selectedContact;
 };
 
 const removeContact = async (contactId) => {
-  contacts = contacts.filter((contact) => contact.id !== id);
+  const data = fs.readFileSync(contactsPath);
+  const allContacts = JSON.parse(data);
+
+  const foundContact = allContacts.find((contact) => contact.id === contactId); // Znaleziony kontakt do usunięcia
+  if (!foundContact) {
+    // Jeżeli nie ma kontaktu, zwracam false i oczywiście przerywamy działanie funkcji
+    return false;
+  }
+
+  // Filtrujemy obecną listę kontaktów BEZ znalezionego kontaktu - otrzymujemy listę tak jakby z usuniętym kontaktem
+  const newContactsList = allContacts.filter(
+    (contact) => contact.id !== foundContact.id
+  );
+  fs.writeFileSync(contactsPath, JSON.stringify(newContactsList));
+  return true;
 };
 
-const addContact = async (body) => {};
+const addContact = async (body) => {
+  const data = fs.readFileSync(contactsPath);
+  const allContacts = JSON.parse(data);
 
-const updateContact = async (contactId, body) => {};
+  // body zawiera name, email i phone, więc wystarczy dodać do obiektu id aby utworzyć nowy kontakt
+  const newContact = { id: nanoid(), ...body };
+  allContacts.push(newContact);
+
+  fs.writeFileSync(contactsPath, JSON.stringify(allContacts));
+
+  return newContact;
+};
+
+const updateContact = async (contactId, body) => {
+  const data = fs.readFileSync(contactsPath);
+  const allContacts = JSON.parse(data);
+
+  const foundContactIndex = allContacts.findIndex(
+    (contact) => contact.id === contactId
+  ); // Znaleziony kontakt do usunięcia
+
+  if (foundContactIndex === -1) {
+    // Jeżeli nie ma kontaktu (funkcja findIndex zwraca -1), zwracam false i oczywiście przerywamy działanie funkcji
+    return false;
+  }
+
+  // Na podstawie pozyskanego wcześniej indeksu w tablicy, modyfikujemy kontakt
+  allContacts[foundContactIndex].name = body.name;
+  allContacts[foundContactIndex].email = body.email;
+  allContacts[foundContactIndex].phone = body.phone;
+
+  fs.writeFileSync(contactsPath, JSON.stringify(allContacts));
+  return allContacts[foundContactIndex];
+};
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,29 +1,77 @@
 const express = require("express");
-const { listContacts } = require("../../models/contacts");
+const Joi = require("joi");
+
+const schema = Joi.object({
+  name: Joi.string().required(),
+  email: Joi.string().email().required(),
+  phone: Joi.string().required(),
+});
+
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} = require("../../models/contacts");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
-  const contacts = listContacts();
+  const contacts = [await listContacts()];
   res.status(200).send({ contacts });
 });
 
 router.get("/:contactId", async (req, res, next) => {
-  const contacts = getContactById();
-  res.status(200).send({ contact });
-  res.status(404).send({ message: "Not found" });
+  // Params to są te po ukośniku w URI, np. http://adres:port/nazwa/funkcji/12345 - gdzie 12345 to contactId
+  const { contactId } = req.params;
+  const contact = await getContactById(contactId);
+  if (contact) {
+    res.status(200).send({ contact });
+  } else {
+    res.status(404).send({ message: "Not found" });
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  // Bez Joi musielibyśmy walidować w ten, a być może nawet bardziej skomplikowany sposób
+  // if (!req.body.name || !req.body.email || !req.body.field) {
+  //   res.status(400).json({ message: "missing required name - field" });
+  // }
+
+  // Z Joi:
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    res.status(400).json({ message: "missing required name - field" });
+  } else {
+    // req.body zawiera dane, które są przekazywane jako body requestu typu POST lub innych typów - odpowiednik parametrów po znaku zapytania (query) albo po ukośniku (params)
+    // tylko pozwalają wprowadzić dużo więcej danych w różnym formacie
+    const newContact = await addContact(req.body);
+    res.status(201).json(newContact); // TODO: dodac wygenerowany obiekt kontaktu
+  }
 });
 
 router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const isContactRemoved = await removeContact(req.params.contactId);
+  if (isContactRemoved) {
+    res.status(200).json({ message: "contact deleted" });
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
 });
 
 router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+  const validationResult = schema.validate(req.body);
+  if (validationResult.error) {
+    res.status(400).json({ message: "missing fields" });
+  } else {
+    const contact = await updateContact(req.params.contactId, req.body);
+    if (contact) {
+      res.status(200).send({ contact });
+    } else {
+      res.status(404).send({ message: "Not found" });
+    }
+  }
 });
 
 module.exports = router;
